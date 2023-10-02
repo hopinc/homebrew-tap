658d2f7d273d1a4248f40837811771bdbb0ece28
hopdeployer <phin+hopdeployer@hop.io>
2023-10-02 23:50:20 +0000

    Update hop.rb for v0.2.55

--- hop.rb
+++ hop.rb
@@ -1,8 +1,8 @@
 class Hop < Formula
   desc "Interact with Hop in your terminal"
   homepage "https://hop.io"
-  url "https://github.com/hopinc/cli/archive/v0.2.54.tar.gz"
-  sha256 "266f2d3ee770ae76eefa8a27f3579f034a51d65372692a07dc46f14f9a9df2a4"
+  url "https://github.com/hopinc/cli/archive/v0.2.55.tar.gz"
+  sha256 "e92f8628cf1c88f3c86187c2e8856aaf20a0338f5b64d3ee79121a7e53946f67"
   license "MPL-2.0"
   head "https://github.com/hopinc/cli.git", branch: "main"
 
